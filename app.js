a99c1b8e36d30e2e73e2f5ca82de7e3076087845
kubat <kubat.aiupov@gmail.com>
2023-02-25 20:43:20 +0100

    add todos to d later on

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 class School {
-  constructor(name,numberOfStudents) {
+  constructor(name, numberOfStudents) {
     this._name = name;
-    this._level = '';
+    this._level = "";
     this._numberOfStudents = numberOfStudents;
   }
   get name() {
@@ -16,10 +16,10 @@ class School {
   }
 
   set numberOfStudents(number) {
-    if (typeof this._numberOfStudents === 'number') {
-      return this._numberOfStudents = number
+    if (typeof this._numberOfStudents === "number") {
+      return (this._numberOfStudents = number);
     } else {
-      console.log('Invalid input: numberOfStudents must be set to a Number.');
+      console.log("Invalid input: numberOfStudents must be set to a Number.");
     }
   }
 
@@ -27,17 +27,18 @@ class School {
     return `${this._name} educates ${this._numberOfStudents} students at the ${this._level} level.`;
   }
 
-  static pickSubstituteTeachers(substitudeTeacher) {
-    let substitudeTeachers = [];
-    const randomIndex = Math.floor(Math.random()* substitudeTeacher.length);
-    return substitudeTeachers[randomIndex]
+  static pickSubstituteTeacher(substitudeTeacher) {
+    // this.substitudeTeacher = [substitudeTeacher];
+    const randomIndex = Math.floor(Math.random() * substitudeTeacher.length);
+    return substitudeTeacher[randomIndex];
   }
+  // TODO: fix this static method
 }
 
 class Primary extends School {
-  constructor(name,numberOfStudents, pickupPolicy) {
-    super(name,numberOfStudents);
-    this._level = 'primary';
+  constructor(name, numberOfStudents, pickupPolicy) {
+    super(name, numberOfStudents);
+    this._level = "primary";
     this._pickupPolicy = pickupPolicy;
   }
   get pickupPolicy() {
@@ -47,15 +48,15 @@ class Primary extends School {
 
 class Middle extends School {
   constructor(name, numberOfStudents) {
-    super(name, numberOfStudents)
-    this._level = 'middle';
+    super(name, numberOfStudents);
+    this._level = "middle";
   }
 }
 
 class High extends School {
   constructor(name, numberOfStudents) {
     super(name, numberOfStudents);
-    this._level = 'high';
+    this._level = "high";
     this._sportsTeams = [];
   }
   get sportsTeams() {
@@ -63,3 +64,32 @@ class High extends School {
   }
 }
 
+const lorrainHansbury = new Primary(
+  "Lorraine Hansbury",
+  514,
+  "Students must be picked up by a parent, guardian, or a family member over the age of 13."
+);
+
+console.log(lorrainHansbury.quickFacts());
+// console.log(
+//   lorrainHansbury.pickSubstituteTeacher([
+//     "Jamal Crawford",
+//     "Lou Williams",
+//     "J. R. Smith",
+//     "James Harden",
+//     "Jason Terry",
+//     "Manu Ginobli",
+//   ])
+// );
+
+const alSmith = new High("Al Smith", 415, [
+  "Baseball",
+  "Basketball",
+  "Volleyball",
+  "Track and Field",
+]);
+
+// TODO:
+// Create a middle school class
+// Add more properties to each class (averageTestScores, schoolOverview, etc.)
+// Create a class called SchoolCatalog that holds a collection of schools. Create an instance of SchoolCatalog for primary, middle, and high schools.
